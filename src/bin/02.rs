1f6198c652c49b9845d7157a754991f713b9532f
meekteek <filledtotofucrumbs@gmail.com>
2023-06-12 16:08:17 -0700

    finish day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,100 @@
+use std::collections::HashMap;
+
+pub fn part_one_input(input: &str) -> Option<u32> {
+    let output = input.split('\n').fold(0, |acc, line: &str| {
+        let rps_tie: HashMap<char, char> = [('A', 'X'), ('B', 'Y'), ('C', 'Z')]
+            .iter()
+            .cloned()
+            .collect();
+        let rps_win: HashMap<char, char> = [('A', 'Y'), ('B', 'Z'), ('C', 'X')]
+            .iter()
+            .cloned()
+            .collect();
+
+        let mut counter = 0;
+        let mut chars = line.chars();
+        let elf_move = chars.next().unwrap();
+        let player_move = chars.skip(1).next().unwrap();
+
+        if rps_tie.get(&elf_move) == Some(&player_move) {
+            counter += 3;
+        }
+        else if rps_win.get(&elf_move) == Some(&player_move)  {
+            counter += 6;
+        }
+        match player_move {
+            'X' => counter += 1,
+            'Y' => counter += 2,
+            'Z' => counter += 3,
+            _ => {}
+        };
+        return acc + counter
+    });
+    return Some(output as u32);
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    return part_one_input(input);
+}
+
+
+pub fn part_two_input (input: &str) -> Option<u32> {
+    let output = input.split('\n').fold(0, |acc, line: &str| {
+        let rps_tie: HashMap<char, char> = [('A', 'X'), ('B', 'Y'), ('C', 'Z')]
+            .iter()
+            .cloned()
+            .collect();
+        let rps_win: HashMap<char, char> = [('A', 'Y'), ('B', 'Z'), ('C', 'X')]
+            .iter()
+            .cloned()
+            .collect();
+       
+        let mut counter = 0;
+        let mut chars = line.chars();
+        let elf_move = chars.next().unwrap();
+        let outcome = chars.skip(1).next().unwrap();
+
+        let player_move = match elf_move {
+            'A' => match outcome {
+                'X' => 'Z',
+                'Y' => 'X',
+                'Z' => 'Y',
+                _ => unreachable!(),
+            },
+            'B' => match outcome {
+                'X' => 'X',
+                'Y' => 'Y',
+                'Z' => 'Z',
+                _ => unreachable!(),
+            },
+            'C' => match outcome {
+                'X' => 'Y',
+                'Y' => 'Z',
+                'Z' => 'X',
+                _ => unreachable!(),
+            },
+            _ => panic!("Invalid shape: {}", elf_move),
+        };
+
+        if rps_tie.get(&elf_move) == Some(&player_move) {
+            counter += 3;
+        }
+        else if rps_win.get(&elf_move) == Some(&player_move)  {
+            counter += 6;
+        }
+        match player_move {
+            'X' => counter += 1,
+            'Y' => counter += 2,
+            'Z' => counter += 3,
+            _ => {}
+        };
+        return acc + counter
+    });
+    return Some(output as u32);
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    part_two_input(input)
 }
 
 fn main() {
@@ -19,12 +110,16 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_one(&input), None);
+        let output = part_one(&input);
+        println!("{}", output.unwrap());
+        assert_ne!(part_one(&input), None);
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        let output = part_two(&input);
+        println!("{}", output.unwrap());
+        assert_ne!(part_two(&input), None);
     }
 }

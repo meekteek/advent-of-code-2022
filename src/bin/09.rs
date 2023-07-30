097ec9cf4ee2ed48185960002060484bd478338b
Teek <filledtotofucrumbs@gmail.com>
2023-07-30 15:07:55 -0700

    finish day 9 pt2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -73,12 +73,75 @@ pub fn parse(input: &str) -> u32 {
     return positions.len() as u32;
 }
 
+pub fn parse_two(input: &str) -> u32 {
+    let mut directions: Vec<Direction> = Vec::new();
+    let mut distances: Vec<i32> = Vec::new();
+
+    let direction_map: HashMap<Direction, (i32, i32)> = [
+        (Direction::Up, (0, 1)),
+        (Direction::Down, (0, -1)),
+        (Direction::Right, (1, 0)),
+        (Direction::Left, (-1, 0)),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    input.lines().for_each(|line| {
+        let chars_in_line: Vec<char> = line.chars().collect();
+        directions.push(match chars_in_line[0] {
+            'U' => Direction::Up,
+            'D' => Direction::Down,
+            'R' => Direction::Right,
+            'L' => Direction::Left,
+            _ => panic!(),
+        });
+        let num_str: String = chars_in_line[2..].iter().collect();
+        distances.push(num_str.parse().unwrap());
+    });
+    let mut rope_pieces: Vec<(i32, i32)> = vec![(0, 0); 10];
+
+    let mut positions: HashMap<(i32, i32), String> = HashMap::new();
+    positions.insert((0, 0), String::from(""));
+
+    // for each direction, move the head and tail
+    for (direction, distance) in directions.iter().zip(distances.iter()) {
+        // have to run through each distance in the list of distances to check each piece of rope
+        for ignore in 0..*distance {
+            rope_pieces[0] = (
+                rope_pieces[0].0 + direction_map[direction].0,
+                rope_pieces[0].1 + direction_map[direction].1,
+            );
+
+            for i in 1..rope_pieces.len() {
+                let (mut tail_left, mut tail_right) = rope_pieces.split_at_mut(i);
+                let (mut tail_parent, mut tail_right) = (tail_left[i - 1], &mut tail_right[0]);
+
+                if tail_parent.1.abs_diff(tail_right.1) > 1
+                    || tail_parent.0.abs_diff(tail_right.0) > 1
+                {
+                    let diff_position =
+                        (tail_right.0 - tail_parent.0, tail_right.1 - tail_parent.1);
+                    let larger_diff = diff_position.0.abs().max(diff_position.1.abs());
+                    let add_diff = (diff_position.0 / larger_diff, diff_position.1 / larger_diff);
+                    *tail_right = (tail_parent.0 + add_diff.0, tail_parent.1 + add_diff.1);
+                    if i == 9 {
+                        positions.insert(*tail_right, String::from(""));
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return positions.len() as u32;
+}
 pub fn part_one(input: &str) -> Option<u32> {
     Some(parse(input))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(parse_two(input))
 }
 
 fn main() {
@@ -100,6 +163,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(9));
     }
 }
